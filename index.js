c792016ab6141965ecf67098224fa685735c9fb9
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-04-23 19:37:41 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
-}
+  
+  if(someNumber<=400){
+   return 'This one is on me!';
+  }else if(someNumber>=400 && someNumber<=2000){
+   return 'That will be twenty bucks.';
+  }else if(someNumber>2000 && someNumber<=2500){
+    return 'I will gladly take your thirty bucks.';
+  }else {
+    return 'No can do.';
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
-
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city==='NYC'){
+    return 'Ok, sounds good.';
+   }else {
+    return 'No go.';
+  }
+}
+let tip="generous";
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  
+  
+  switch(tip){
+    case 'generous': 
+    return "Thank you so much."; 
+    break;
+    case 'not as generous': 
+    return "Thank you."; 
+    break;
+    default:
+    return "Bye."; 
+    break;
+    
+     
+  }
 }
